aea75ad1b5a25be3db3f2af3fac4acc682c42dc0
Filipe Resendes <filipecr2001@gmail.com>
2026-03-28 17:09:30 +0100

    Initial Commit

---
+++ ourspace-app/ourspace-app/ourspace_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ourspace_appApp.swift
+//  ourspace-app
+//
+//  Created by Filipe Resendes on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ourspace_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
